619e261429d872367a12866a95f087c0791feedb
Dat Quang <daoquangdat0103@gmail.com>
2025-03-27 21:26:24 +0700

    fix login token with layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,15 +1,31 @@
 import Footer from "./footer/Footer";
 import MobileFooter from "./footer/MobileFooter";
 import Navbar from "./navbar/Navbar";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 // eslint-disable-next-line react/prop-types
 const Layout = ({ children }) => {
+  const dispatch = useDispatch();
+  const { userInfo } = useSelector((state) => state.userLogin);
+
+  useEffect(() => {
+    const storedUserInfo = localStorage.getItem("userInfo");
+    if (storedUserInfo && !userInfo) {
+      const userData = JSON.parse(storedUserInfo);
+      // Khôi phục trạng thái đăng nhập bằng cách dispatch USER_LOGIN_SUCCESS
+      dispatch({
+        type: "USER_LOGIN_SUCCESS",
+        payload: userData,
+      });
+    }
+  }, [dispatch, userInfo]);
+
   return (
     <div className="bg-main text-white">
       <Navbar />
       {children}
       <Footer />
-      {/* Mobile footer */}
       <MobileFooter />
     </div>
   );
